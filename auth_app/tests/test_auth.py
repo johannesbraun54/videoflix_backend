7016474e1f79ef30345c9319704a9d3b4940838f
johannesbraun54 <johannesbraun02@gmail.com>
2025-07-02 09:59:30 +0200

    added tests for email_avialability_check

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -7,8 +7,23 @@ class RegistartionTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_check_existing_email(self):
+    def test_check_new_email_availability(self):
         url = reverse('email_availability')
         new_email = 'example@mail.de'
-        reponse = self.client.post(url, {'email': new_email})
-        self.assertEqual(reponse.status_code, status.HTTP_200_OK)
+        response = self.client.post(url, {'email': new_email})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'exists': False})
+        
+    def test_check_existing_email_availability(self):
+        url = reverse('email_availability')
+        exisisting_email = 'example@mail.de'
+        User.objects.create_user(username='testuser', email=exisisting_email, password='testpassword')
+        response = self.client.post(url, {'email': exisisting_email})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'exists': True})
+        
+    def test_check_bad_request(self):
+        url = reverse('email_availability')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "Email is required"})
