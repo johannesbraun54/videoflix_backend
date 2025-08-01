ff301ac44abdd1c5bf5884c199032e69a5b55a3e
johannesbraun54 <johannesbraun02@gmail.com>
2025-08-01 18:49:19 +0200

    refactor: added convert_480p to signals

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,12 +1,14 @@
 from .models import Video
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
+from .tasks import convert_480p
 import os
 
 @receiver(post_save, sender=Video)
 def video_post_safe(sender, instance, created, **kwargs):
     print("video wurde gespeichert")
     if created:
+        convert_480p(instance.file.path)
         print("video created")
         
 # @receiver(post_delete, sender=Video)
@@ -20,9 +22,6 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     Deletes file from filesystem
     when corresponding `Video` object is deleted.
     """
-    
-    print("DELETE FROM DB")
     if instance.file:
         if os.path.isfile(instance.file.path):
             os.remove(instance.file.path)
-            print("DELETE FROM OS")
