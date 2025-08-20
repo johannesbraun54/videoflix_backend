58b320571a1a23010ed7f8920820daf8acff1bd4
johannesbraun54 <johannesbraun02@gmail.com>
2025-08-20 18:48:33 +0200

    fix: fixed bugs at test_videos.py

--- videos/tests/test_videos.py
+++ videos/tests/test_videos.py
@@ -1,8 +1,34 @@
 import pytest
 from ..models import Video
 from django.urls import reverse
+from django.contrib.auth.models import User
+from auth_app.models import Userprofile
+    
+@pytest.fixture
+def token_obtain_pair_url():
+    return reverse('token_obtain_pair')
 
-def test_video_get(client):
+@pytest.fixture
+def test_user():
+    return User.objects.create_user(
+        username='user',
+        email="user@example.com",
+        password='testpassword'
+    )
+    
+@pytest.fixture
+def test_userprofile(test_user):
+    return Userprofile.objects.create(
+        user=test_user,
+        username=test_user.username,
+        email=test_user.email,
+        is_verified=True
+    )
+
+@pytest.mark.django_db
+def test_get_video_list(client, token_obtain_pair_url, test_userprofile):
+    login_data = {"email": test_userprofile.email, "password": "testpassword"}
+    response = client.post(token_obtain_pair_url, login_data, content_type="application/json")
     url = reverse("videos")
     response = client.get(url)
     assert response.status_code == 200
