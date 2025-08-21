d8d712da52641bc2a8f917c550cb5d76b1781e8a
johannesbraun54 <johannesbraun02@gmail.com>
2025-08-21 09:18:32 +0200

    feat(videos): added task generate_thumbnail() and delete thumbnail

--- videos/signals.py
+++ videos/signals.py
@@ -1,17 +1,18 @@
 from .models import Video
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
-from .tasks import convert_480p
+from .tasks import convert_480p, generate_thumbnail
 import os
 import django_rq
+from django.conf import settings
+
 
 @receiver(post_save, sender=Video)
 def video_post_safe(sender, instance, created, **kwargs):
-    print("video wurde gespeichert")
     if created:
         queue = django_rq.get_queue("default", autocommit=True)
         queue.enqueue(convert_480p, instance.file.path)
-        print("video created")
+        queue.enqueue(generate_thumbnail, instance.id)
     
     
 @receiver(post_delete, sender=Video)
@@ -21,5 +22,11 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     when corresponding `Video` object is deleted.
     """
     if instance.file:
+        thumbnail_path = os.path.join(settings.MEDIA_ROOT, "thumbnails", f"{instance.id}.jpg")
         if os.path.isfile(instance.file.path):
             os.remove(instance.file.path)
+            print("(instance.file",instance.file)
+        if os.path.isfile(thumbnail_path):
+            os.remove(thumbnail_path)
+
+### delete all convert versions 
--- videos/tasks.py
+++ videos/tasks.py
@@ -1,7 +1,37 @@
 import subprocess
+import os
+import subprocess
+from django.conf import settings
+from .models import Video
 
 def convert_480p(source):
     new_file_name = source[:-4] + "_480p.mp4"
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-    print("lääuufftt")
     subprocess.run(cmd,shell=True)
+    
+    
+
+def generate_thumbnail(video_id):
+    video = Video.objects.get(id=video_id)
+    input_path = video.file.path
+
+    output_dir = os.path.join(settings.MEDIA_ROOT, "thumbnails") 
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, f"{video.id}.jpg") 
+
+    command = [
+        "ffmpeg",                   # Aufruf von ffmpeg (Videoverarbeitungsprogramm)
+        "-ss", "00:00:05.000",      # Springe zu 5 Sekunden im Video
+        "-i", input_path,           # Eingabevideo (Pfad zur Videodatei)
+        "-vframes", "1",            # Erzeuge genau 1 Einzelbild
+        "-q:v", "2",                # Bildqualität (2 = sehr gut, kleiner = besser)
+        output_path,                # Ausgabepfad für das Thumbnail
+        "-update", "1",         # wichtig für Docker: überschreibt existierende Datei
+    ]
+
+
+    subprocess.run(command, check=True)
+
+    # Pfad im Model speichern
+    video.thumbnail_url = f"http://127.0.0.1:8000/media/thumbnails/{video.id}.jpg"
+    video.save()
