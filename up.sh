b55c391c4227e81c3814aeced2d94530741a52cb
johannesbraun54 <johannesbraun02@gmail.com>
2025-08-26 13:40:44 +0200

    refactor:changed host port

---
+++ up.sh
@@ -0,0 +1,6 @@
+git pull
+git add .
+git commit -m "$*"
+git push
+git ftp push         
+# ssh junus@34.65.211.91 "cd da-services && sudo git pull && sudo ./env/bin/pip install -r requirements.txt"
